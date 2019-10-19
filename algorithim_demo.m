0f2d7d61f3a4b5dad77ba90ae12cbb34e8aad034
louisrosenblum <42879481+louisrosenblum@users.noreply.github.com>
2019-10-18 22:20:26 -0600

    Added distance function

--- algorithim_demo.m
+++ algorithim_demo.m
@@ -17,8 +17,19 @@ grid = cell(100,100);
 % Populate grid points with x,y coordinates (center of each grid point)
 for i = 1:100
     for j = 1:100
-    grid{i,j} = [ (10*i-5) (10*i+995)];     
+    grid{i,j} = [ (10*i-5) (10*j+995)];     
     end
+end
+
+%% Distance usage example
+
+dist1 = distance(s0,grid{90,30})
+
+
+%% Distance function definition
+
+function dist = distance(p1,p2)
+    dist = sqrt(abs((p2(1) - p1(1))^2 + (p2(2)-p1(2))^2));
 end
 
 
